9ce863e716a3d94b17a7c327635ca19b5cd65b39
daniel komoe <daniel.komoe@epitech.eu>
2024-10-17 11:41:29 +0200

    [ADD] handled core error problem

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -14,12 +14,21 @@ exports.handler = async function(event, context) {
     const response = await axios.get(url);
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Autoriser toutes les origines
+        'Access-Control-Allow-Methods': 'GET, OPTIONS', // Méthodes autorisées
+        'Content-Type': 'application/json' // Type de contenu
+      },
       body: JSON.stringify(response.data)
     };
   } catch (error) {
+    console.error('Erreur lors de la récupération des données:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Erreur lors de la récupération des données depuis l\'API externe' })
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Assurez-vous d'ajouter les en-têtes CORS ici aussi
+      },
+      body: JSON.stringify({ error: 'Erreur lors de la récupération des données depuis l\'API externe', details: error.message })
     };
   }
 };
